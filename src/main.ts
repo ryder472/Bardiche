55da44d4ed28c3f3983ad2d1d0f403d404b1dfc7
ryder472 <gtafansa@gmail.com>
2023-11-22 12:14:09 +0000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+// 必要なパッケージをインポートする
+import { GatewayIntentBits, Client, Partials, Message } from 'discord.js'
+import dotenv from 'dotenv'
+
+// .envファイルを読み込む
+dotenv.config()
+
+// Botで使うGatewayIntents、partials
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Message, Partials.Channel],
+})
+
+// Botがきちんと起動したか確認
+client.once('ready', () => {
+  console.log('Ready!')
+  if (client.user != null) {
+    console.log(client.user.tag)
+  }
+})
+
+// !timeと入力すると現在時刻を返信するように
+client.on('messageCreate', async (message: Message) => {
+  if (message.author.bot) return
+  if (message.content?.match('/(twitter|x)\.com/')) {
+    const fixMessage = message.content.replace(/(twitter|x)\.com/, 'vxtwitter.com')
+    message.channel.send(fixMessage)
+  }
+})
+
+// ボット作成時のトークンでDiscordと接続
+client.login(process.env.TOKEN)
